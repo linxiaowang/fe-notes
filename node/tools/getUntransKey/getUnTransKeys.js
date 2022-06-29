b05d628d7a6a625eab991df0fd340d07ab5b4227
linxiaowang <1104220717@qq.com>
2022-06-29 10:41:57 +0800

    feat: update tools

---
+++ node/tools/getUntransKey/getUnTransKeys.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const zh = require('./zh.js');
+console.log(typeof zh);
+const existKeys = Object.keys(zh)
+let filePath =  path.resolve(__dirname);
+
+scanFolder(filePath);
+
+
+function scanFolder(dirPath){
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    files.forEach(file => {
+      const filePath = path.join(dirPath, file);
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        if (stats.isFile()) {
+          fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            const result = getString(data);
+            if (result) {
+              printResult = result.filter(item => {
+                return existKeys.indexOf(item) === -1;
+              });
+              fs.writeFile('result.txt', printResult, (err) => {
+                if (err) {
+                  console.log(err);
+                  return;
+                }
+                console.log('写入成功');
+              });
+            }
+              console.log(result);
+          });
+        } else {
+          scanFolder(filePath);
+        }
+      });
+    });
+  });
+}
+
+
+function getString(str){
+  const reg = /(?<=\$t\(').*?(?='\))/g;
+  const result = str.match(reg);
+  if (result) {
+    return result;
+  }
+}
